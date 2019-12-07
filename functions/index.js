4cc202409e2e272948800370247435c02a95cba7
cris <criston2011@gmail.com>
2019-12-07 14:53:52 +0530

    :)

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const functions = require("firebase-functions");
+
+// environment variables
+require("dotenv").config();
+exports.tweets = functions.https.onCall(async body => {
+  try {
+    const Twitter = require("twitter");
+    const client = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+    const params = { q: body.q, count: 10 };
+    return await new Promise((resolve, reject) => {
+      client.get("search/tweets", params, (error, tweets, response) => {
+        if (!error) {
+          return resolve(tweets);
+        } else {
+          return reject(error);
+        }
+      });
+    });
+  } catch (e) {
+    console.error(e);
+  }
+  return this.tweets;
+});
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appID: '03b8b67f9130defffb755734db096a9a',
+  gKey: '',
+  config: {
+    apiKey: 'AIzaSyBnOnU2yZPNwG-bhobRv9jBziQaIV5licQ',
+    authDomain: 'utility-dashboard-b24c5.firebaseapp.com',
+    databaseURL: 'https://utility-dashboard-b24c5.firebaseio.com',
+    projectId: 'utility-dashboard-b24c5',
+    storageBucket: 'utility-dashboard-b24c5.appspot.com',
+    messagingSenderId: '925704430699'
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
